43ae29ebc31ae1ebbb562624684f78272c4c583c
Priti <142306089+priti-dhekne@users.noreply.github.com>
2023-08-15 16:21:28 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,12 @@
 #write a python program to read a string as an input and check given string is palindrome or not
-s="wasitacarorcatisaw"
+str=(input())
+
+reverse=str[::-1]
+if reverse==str:
+    print(reverse,"is a palindrome")
+else:
+    print(reverse,"is not palindrome")    
+
 
 
 
